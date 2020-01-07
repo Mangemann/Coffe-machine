b0ee92c3e92095216a45011ce6980417ef74f2f5
Jens <jens.i.t.magnusson@hotmail.com>
2020-01-07 14:32:51 +0100

    Added desc
    Added description of the överall design of the coffee machine

---
+++ index.js
@@ -0,0 +1,6 @@
+class MyCoffeeMachine{
+    
+}
+
+
+module.exports = MyCoffeeMachine;
---
+++ step-definitions/coffee-machine.js
@@ -0,0 +1,2 @@
+let coffeeMachine = require('../index.js') //man skriver "../" för att 'hoppa upp' från mappen man är i
+let myMachine = new coffeeMachine();
