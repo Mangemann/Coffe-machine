f6ca0452e2c70a76a9d12a8251241353f45b9c11
Jens <jens.i.t.magnusson@hotmail.com>
2020-01-10 10:04:14 +0100

    Base of class
    Base for the coffee machine class created and filled with all properties and empty methods

--- index.js
+++ index.js
@@ -1,11 +1,168 @@
-class MyCoffeeMachine{
+class MyMachine{
 
     constructor() {
-        this
+        this.pluggedInWater = false
+        this.pluggedInPower = false
+        this.waterPerCup = 180
+        this.waterPerKanna = 1800
+        this.waterPerLatte = 60
+        this.coffeePerCup = 9
+        this.coffePerLatteCup = 3
+        this.milkPerCup = 10
+        this.milkPerLatteCup = 120
+        this.coffeePerKanna = 90
+        this.sugarPerCup = 10
+        this.chocolatePerCup = 10
+        this.amountofCoffee = 0
+        this.amountofMilk = 0
+        this.amountofSugar = 0
+        this.amountofChocolate = 0
+        this.latteButtonPressed = false
+        this.kaffeButtonPressed = true
+        this.teButtonPressed = false
+        this.chokladButtonPressed = false
+        this.sockerButtonPressed = false
+        this.mjolkButtonPressed = false
+        this.kannaButtonPressed = false
+        this.amountOfMoneyPaid = 0
+        this.pricePerCup = 10
+        this.amountOfCups = 0
+
     }
+
+    checkIfPluggedIn() {
+        
+    }
+
+    checkIfWaterIsPluggedIn() {
+
+    }
+
+    coffeeSupplyLeft() {
+
+    }
+
+    milkSupplyLeft() {
+
+    }
+
+    sugarSupplyLeft() {
+
+    }
+
+    chocolateSupplyLeft() {
+
+    }
+
+    cupSupplyLeft() {
+
+    }
+
+    start() {
+
+    }
+
+    cancel() {
+
+    }
+
+    brewBeverage() {
+
+    }
+
+    brewCoffeeMilkSugar() {
+
+    }
+
+    brewCoffeeMilk() {
+
+    }
+
+    brewCoffeeSugar() {
+
+    }
+
+    brewCoffee() {
+
+    }
+
+    brewTe() {
+
+    }
+
+    brewLatte() {
+
+    }
+
+    brewKanna() {
+
+    }
+
+    brewChocolate() {
+
+    }
+
+    returnChange() {
+
+    }
+
+    payByCard() {
+
+    }
+
+    payByCash() {
+
+    }
+
+    selectSugar() {
+
+    }
+
+    selectMilk() {
+
+    }
+
+    selectKanna() {
+
+    }
+
+    selectCoffee() {
+
+    }
+
+    selectLatte() {
+
+    }
+
+    selectTe() {
+
+    }
+
+    selectChocolate() {
+
+    }
+
+    refillSugar() {
+
+    }
+
+    refillMilk() {
+
+    }
+
+    refillCoffee() {
+
+    }
+
+    refillChocolate() {
+        
+    }
+
     
+
+
 
 }
 
 
-module.exports = MyCoffeeMachine;
+module.exports = MyMachine;
